1677bbfcd19ec91c58530191b9f3eb1d1994b776
Petri Kallio <kallionpetri@gmail.com>
2018-08-30 08:19:28 +0300

    tehtävät 2.4-2.5

--- kurssit/src/App.js
+++ kurssit/src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Kurssi from './components/Kurssi'
 
 const App = () => {
-    const kurssi = {
+  const kurssit = [
+    {
       nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
       osat: [
         {
           nimi: 'Reactin perusteet',
@@ -21,11 +23,29 @@ const App = () => {
           id: 3
         }
       ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
     }
+  ]
   
     return (
       <div>
-        <Kurssi kurssi={kurssi} />
+        <h1>Opetusohjelma</h1>
+        {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
       </div>
     )
 }
